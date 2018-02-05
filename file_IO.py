5531f2625bf8844e89231406c094a3ad7cef8587
kostiantyn <chercas.kostya@gmail.com>
2018-02-05 18:17:02 +0200

    Was added some features

--- file_IO.py
+++ file_IO.py
@@ -1,6 +1,11 @@
-jabber = open('sample.txt', 'r')
-
-for line in jabber:
-    print(line)
-
-jabber.close()
+# jabber = open('sample.txt', 'r')
+#
+# for line in jabber:
+#     if 'jabberwock' in line.lower():
+#         print(line, end='')
+#
+# jabber.close()
+with open('sample.txt', 'r') as jabber:
+    for line in jabber:
+        if 'JAB' in line.upper():
+            print(line, end='')
